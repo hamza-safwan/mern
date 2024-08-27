38b34b19b519204a5d0ed30d6e8d7fca12b408a6
Hamza Safwan <119676651+hamza-safwan@users.noreply.github.com>
2024-08-27 22:16:37 +0500

    modified app routing script

--- rest.js
+++ rest.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Second Chance');
 });
 
 app.listen(port, () => {
